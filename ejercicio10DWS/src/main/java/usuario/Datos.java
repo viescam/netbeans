4804e8c91ee8a1ab9ac01fe7cf1f73235f62e0d9
viescam <viescam@outlook.es>
2016-09-28 20:43:33 +0200

    Modificación del ejercicio10DWS

---
+++ ejercicio10DWS/src/main/java/usuario/Datos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuario;
+
+
+public class Datos {
+    private String nombreUsuario;
+    private String claveAcceso;
+
+    /**
+     * @return the nombreUsuario
+     */
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    /**
+     * @param nombreUsuario the nombreUsuario to set
+     */
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    /**
+     * @return the claveAcceso
+     */
+    public String getClaveAcceso() {
+        return claveAcceso;
+    }
+
+    /**
+     * @param claveAcceso the claveAcceso to set
+     */
+    public void setClaveAcceso(String claveAcceso) {
+        this.claveAcceso = claveAcceso;
+    }
+}
